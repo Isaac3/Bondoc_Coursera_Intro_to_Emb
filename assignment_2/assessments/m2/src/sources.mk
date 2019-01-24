cf8bf7278b85baddd9d299bd1b9574b29106a82e
Isaac Bondoc <isaac.bondoc@gmail.com>
2019-01-24 11:37:07 -0600

    updates sources and includes. Also added arch specific flags

--- assignment_2/assessments/m2/src/sources.mk
+++ assignment_2/assessments/m2/src/sources.mk
@@ -10,8 +10,14 @@
 #*****************************************************************************
 
 # Add your Source files to this variable
-SOURCES =
+SOURCES = startup_msp432p401r_gcc.c	 \
+	  interrupts__msp432p401r_gcc.c	 \
+	  system_msp432p401r.c		 \
+	  main.c			 \
+	  memory.c
 
 # Add your include paths to this variable
-INCLUDES = 
+INCLUDES =  -I./CMSIS	 \
+	    -I./common	 \
+	    -I./msp432	 
 
